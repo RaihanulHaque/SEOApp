1fa582ec693444693b651dea2a505133efb3cdfb
Rahi <raihanulhaque007@gmail.com>
2022-04-25 11:09:14 +0600

    dhdgfgshf

--- SERP/urls.py
+++ SERP/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.search, name='search'),
+    path("result/", views.result, name='result'),
 ]
--- SERP/views.py
+++ SERP/views.py
@@ -1,43 +1,62 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from . import dict
 from . client import RestClient
-# Create your views here.
-# Test
 
 
 def search(request):
-    if request.method == "POST":
-        client = RestClient()
-        post_data = {}
-        keyword = request.POST.get("keyword")
-        # keyword = input("Enter: ")
-        print(keyword)
-        post_data[len(post_data)] = {
-            "language_code": "en",
-            "location_code": 2840,
-            "keyword": keyword
-        }
-        response = client.post(
-            "/v3/serp/google/organic/live/advanced", post_data)
-        if response["status_code"] == 20000:
-            items = response['tasks'][0]['result'][0]['items']
-            i = 0
-            count = 0
-            while count < 10:
-                if "title" not in items[i] or ("image" or "video") in items[i]["type"]:
-                    del items[i]
-                    i = i+1
-                else:
-                    i = i+1
-                count = count+1
-            for i in range(10, len(items)):
-                del items[10]
-            return render(request, "serp-fetched.html", {'items': items})
+    try:
+        if request.method == "POST":
+            client = RestClient()
+            post_data = {}
+            keyword = request.POST.get("keyword")
+            print(keyword)
+            post_data[len(post_data)] = {
+                "language_code": "en",
+                "location_code": 2840,
+                "keyword": keyword
+            }
+            response = client.post(
+                "/v3/serp/google/organic/live/advanced", post_data)
+            if response["status_code"] == 20000:
+                global items0, items
+                items = []
+                items0 = response['tasks'][0]['result'][0]['items']
+                i = 0
+                count = 0
+                while count < 10:
+                    if "title" not in items0[i] or "organic" not in items0[i].values():
+                        i = i+1
+                        continue
+                    else:
+                        items.append(items0[i])
+                        i = i+1
+                        count = count+1
+                # items = [titems for i in range(0, 10)]
+                # print(items)
+                # i = 0
+                # count = 0
+                # while count < 10:
+                #     if "title" not in items[i] or "organic" not in items[i].values():
+                #         del items[i]
+                #         i = i+1
+                #     else:
+                #         i = i+1
+                #     count = count+1
+                # for i in range(10, len(items)):
+                #     del items[10]
+                return redirect("result")
+            else:
+                messages.error(
+                    request, f"{response['status_code']} {response['status_message']}")
+                return redirect("/serp")
         else:
-            messages.error(
-                request, f"{response['status_code']} {response['status_message']}")
-            return redirect("serp")
+            return render(request, "serp-call.html")
+    except:
+        return redirect("/serp")
 
-    else:
-        return render(request, "serp-call.html")
+
+def result(request):
+    try:
+        return render(request, "serp-fetched.html", {'items': items})
+    except:
+        return redirect("/serp")
